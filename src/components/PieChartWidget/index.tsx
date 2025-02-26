390fbf3cf2714e4552e27cb8059e6818c329d7f1
Trent McDole <trentmcdole94@gmail.com>
2025-02-26 16:29:12 -0600

    fix: pie chart control poisition

--- src/components/PieChartWidget/index.tsx
+++ src/components/PieChartWidget/index.tsx
@@ -3,6 +3,7 @@ import "./styles.scss";
 import { PieChart } from "../pie-chart";
 import { mockPieChartData } from "../pie-chart/utils";
 import { WidgetContainerProps } from "../widget-container";
+import classNames from "classnames";
 
 export const PieChartWidget = ({
   counters,
@@ -31,6 +32,13 @@ export const PieChartWidget = ({
   const dataset = !!counterData?.length ? counterData : mockPieChartData;
 
   const handleSetPieCounters = (newCounter: string) => {
+    if (pieCounters?.[widget.id]?.includes(newCounter)) {
+      setPieCoutners({
+        ...pieCounters,
+        [widget.id]: pieCounters[widget.id].filter((c) => c !== newCounter),
+      });
+      return;
+    }
     setPieCoutners({
       ...pieCounters,
       [widget.id]: [...(pieCounters[widget.id] || []), newCounter],
@@ -40,19 +48,37 @@ export const PieChartWidget = ({
   const counterOptions = useMemo(
     () =>
       Object.keys(counters).filter((key) =>
-        pieCounters
-          ? !counterData?.find((d) => d.id === key) && counterTitles[key]
-          : true
+        pieCounters ? counterTitles[key] : true
       ),
     [pieCounters, counters, counterData, counterTitles]
   );
 
   return (
     <div className="widget-chart-container">
-      <button className="open" onClick={() => setModal(!modal)}>
-        options
-      </button>
-      <div style={{ display: "flex", justifyContent: "center" }}>
+      <div className="modal-container">
+        <button className="open" onClick={() => setModal(!modal)}>
+          <span>options</span>
+          <span>&#129171;</span>
+        </button>
+        {modal && (
+          <div className="modal">
+            {counterOptions.map((key) => (
+              <button
+                onClick={() => handleSetPieCounters(key)}
+                className={classNames(
+                  "modal-option",
+                  counterData?.find((d) => d.id === key) && "active"
+                )}
+                key={key}
+                value={key}
+              >
+                {counterTitles[key]}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="chart-container">
         <PieChart<(typeof dataset)[0]>
           margin={{ top: 2, bottom: 40, left: 0, right: 0 }}
           width={
@@ -67,20 +93,6 @@ export const PieChartWidget = ({
           getValue={(data) => data.value}
         />
       </div>
-      {modal && (
-        <div className="modal">
-          <div className="modal-content">
-            <select onChange={(e) => handleSetPieCounters(e.target.value)}>
-              <option value="">Select a counter</option>
-              {counterOptions.map((key) => (
-                <option key={key} value={key}>
-                  {counterTitles[key]}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
